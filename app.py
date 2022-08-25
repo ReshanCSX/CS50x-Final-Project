68fa0c4e7fdf841c81dc745b0b8bff5382d98f10
ReshanCSX <reshan.luck@gmail.com>
2022-08-25 13:43:30 +0530

    Removed the title from home route and add sessions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, render_template, request, url_for, flash
+from flask import Flask, redirect, render_template, request, url_for, flash, session
+from flask_session import Session
 from cs50 import SQL
 from forms import RegistrationForm, LoginForm
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -12,13 +13,18 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SECRET_KEY"] = 'f545642775718c11360a4aba2429eed1'
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 # Database configuration
 db = SQL("sqlite:///")
 
 @app.route("/")
 def index():
     
-    return render_template("index.html", title="SPLIT")
+    return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
