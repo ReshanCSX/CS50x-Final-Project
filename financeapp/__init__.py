52cbfc923879cf7086ff840bed14a2238e936164
ReshanCSX <reshan.luck@gmail.com>
2022-08-30 17:27:24 +0530

    Added user login support

--- financeapp/__init__.py
+++ financeapp/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 #app configuration
 app = Flask(__name__)
@@ -17,5 +18,10 @@ Session(app)
 # Database configuration
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+# User login handeling
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 from financeapp import routes
--- financeapp/routes.py
+++ financeapp/routes.py
@@ -1,37 +1,53 @@
-from flask import redirect, render_template, url_for, flash, session
+from flask import redirect, render_template, url_for, flash, request
 from financeapp import app, db
 from financeapp.models import User, Account, Transactions
 from financeapp.forms import RegistrationForm, LoginForm
 from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, current_user, login_required
 
 @app.route("/")
 def index():
-    
     return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
 
+    # Redirecting the user to homepage if the user logged in
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+
+    # Rendering the form
     form = LoginForm()
 
+    # Validation
     if form.validate_on_submit():
-        # To test the validation process
-        if form.email.data == "reshan@gmail.com" and form.password.data == "1":
-            flash("Login successful!", "success")
-            return redirect(url_for("home"))
+        user = User.query.filter_by(email=form.email.data).first()
 
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash("Login successful!", "success")
+
+            # Check if the user comes into from a different page
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("home"))
         else:
-            flash("Login unsuccessfull, Please check username and password", "danger")    
+            flash("Login unsuccessfull, Please check email and password", "danger")    
 
     return render_template("login.html", title = "Login", form = form)
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
 
+    # Redirecting the user to homepage if the user logged in
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+
+    # Rendering the form
     form = RegistrationForm()
 
+    # Validation
     if form.validate_on_submit():
-        
+
         # Password Hashing
         hashed_password = generate_password_hash(form.password.data)
 
@@ -46,6 +62,7 @@ def signup():
     return render_template("register.html", title = "Sign Up", form = form)
 
 @app.route("/home", methods=["GET", "POST"])
+@login_required
 def home():
 
     return render_template("overview.html", title = "Overview",)
