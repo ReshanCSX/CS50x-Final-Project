d001c76b27170295887ef08008d297b245f19e17
ReshanCSX <reshan.luck@gmail.com>
2022-12-11 10:19:10 +0530

    Fixed circuler import bug

--- financeapp/__init__.py
+++ financeapp/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from financeapp.helpers import usd
 
 #app configuration
 app = Flask(__name__)
@@ -26,8 +25,8 @@ login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
 
+from financeapp import routes
+from financeapp.helpers import usd
+
 # Custom filter
 app.jinja_env.filters["usd"] = usd
-
-
-from financeapp import routes
